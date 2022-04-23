fc9a81fac8ff8158cd7b9abb09d51c4799fef2e1
De-ad <75694495+De-ad@users.noreply.github.com>
2022-04-23 21:56:07 +0300

    pagination logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,15 +5,131 @@ import { Web3 } from './pages/Web3/Web3';
 import Header from './components/Header';
 import Page from './components/Page';
 import Scroll from './components/Scroll';
+import { useState } from 'react';
+
+const products = [
+  {
+    id: 1,
+    name: 'floppa',
+    description: 'a very nice loved cat',
+    href: '#',
+    price: '$48',
+    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
+    imageAlt: '??',
+
+  },
+  {
+    id: 2,
+    name: 'floppa2',
+    href: '#',
+    price: '$35',
+    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Gregory_Caracal.jpg',
+    imageAlt: '?',
+  },
+  {
+    id: 3,
+    name: 'floppa3',
+    href: '#',
+    price: '$89',
+    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/Big_Floppa_and_Justin_2_%28cropped%29.jpg',
+    imageAlt: '?',
+  },
+  {
+    id: 4,
+    name: 'floppa4',
+    href: '#',
+    price: '$35',
+    imageSrc: 'https://i1.sndcdn.com/artworks-vog58LBUoWgkjgPZ-6Ru4Hg-t500x500.jpg',
+    imageAlt: '?',
+  },
+  {
+    id: 5,
+    name: 'floppa',
+    href: '#',
+    price: '$48',
+    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
+    imageAlt: '??',
+  },
+  {
+    id: 6,
+    name: 'floppa',
+    href: '#',
+    price: '$48',
+    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
+    imageAlt: '??',
+  },
+  {
+    id: 7,
+    name: 'floppa',
+    href: '#',
+    price: '$48',
+    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
+    imageAlt: '??',
+  },
+  {
+    id: 8,
+    name: 'floppa',
+    href: '#',
+    price: '$48',
+    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
+    imageAlt: '??',
+  },
+  {
+    id: 9,
+    name: 'floppa',
+    href: '#',
+    price: '$48',
+    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
+    imageAlt: '??',
+  },
+  {
+    id: 10,
+    name: 'floppa',
+    href: '#',
+    price: '$48',
+    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
+    imageAlt: '??',
+  },
+  {
+    id: 11,
+    name: 'floppa2',
+    href: '#',
+    price: '$35',
+    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Gregory_Caracal.jpg',
+    imageAlt: '?',
+  },
+  {
+    id: 12,
+    name: 'floppa2',
+    href: '#',
+    price: '$35',
+    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Gregory_Caracal.jpg',
+    imageAlt: '?',
+  },
+]
+
 function App() {
+  const [offset, setOffset] = useState(0);
+
+
+
+
   return (
-    
+
     <>
-    <Header />
-    <Page />
-    <Scroll />
-    
-    
+      <Header />
+      <Page
+        offset={offset}
+        products={products}
+      />
+      <Scroll
+        items={products}
+        itemsPerPage={8}
+        setOffset={setOffset}
+        offset={offset + 1}
+      />
+
+
       <ul>
         <li>
           <Link to="/">Main</Link>
--- frontend/src/components/Page.tsx
+++ frontend/src/components/Page.tsx
@@ -8,113 +8,11 @@ function classNames(...classes: string[]) {
 }
 
 
-const products = [
-  {
-    id: 1,
-    name: 'floppa',
-    description: 'a very nice loved cat',
-    href: '#',
-    price: '$48',
-    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
-    imageAlt: '??',
-    
-  },
-  {
-    id: 2,
-    name: 'floppa2',
-    href: '#',
-    price: '$35',
-    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Gregory_Caracal.jpg',
-    imageAlt: '?',
-  },
-  {
-    id: 3,
-    name: 'floppa3',
-    href: '#',
-    price: '$89',
-    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/Big_Floppa_and_Justin_2_%28cropped%29.jpg',
-    imageAlt: '?',
-  },
-  {
-    id: 4,
-    name: 'floppa4',
-    href: '#',
-    price: '$35',
-    imageSrc: 'https://i1.sndcdn.com/artworks-vog58LBUoWgkjgPZ-6Ru4Hg-t500x500.jpg',
-    imageAlt: '?',
-  },
-  {
-    id: 5,
-    name: 'floppa',
-    href: '#',
-    price: '$48',
-    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
-    imageAlt: '??',
-  },
-  {
-    id: 6,
-    name: 'floppa',
-    href: '#',
-    price: '$48',
-    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
-    imageAlt: '??',
-  },
-  {
-    id: 7,
-    name: 'floppa',
-    href: '#',
-    price: '$48',
-    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
-    imageAlt: '??',
-  },
-  {
-    id: 8,
-    name: 'floppa',
-    href: '#',
-    price: '$48',
-    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
-    imageAlt: '??',
-  },
-  {
-    id: 9,
-    name: 'floppa',
-    href: '#',
-    price: '$48',
-    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
-    imageAlt: '??',
-  },
-  {
-    id: 10,
-    name: 'floppa',
-    href: '#',
-    price: '$48',
-    imageSrc: 'https://i.kym-cdn.com/entries/icons/original/000/034/421/cover1.jpg',
-    imageAlt: '??',
-  },
-  {
-    id: 11,
-    name: 'floppa2',
-    href: '#',
-    price: '$35',
-    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Gregory_Caracal.jpg',
-    imageAlt: '?',
-  },
-  {
-    id: 12,
-    name: 'floppa2',
-    href: '#',
-    price: '$35',
-    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Gregory_Caracal.jpg',
-    imageAlt: '?',
-  },
-]
-
-
-export default function Page() {
+export default function Page({ products, offset }) {
   const [open, setOpen] = useState(false)
   const [activeElement, setActiveElement] = useState(0)
 
-  function show(i: SetStateAction<number>){
+  function show(i: SetStateAction<number>) {
     setActiveElement(i)
     setOpen(true)
   }
@@ -127,17 +25,17 @@ export default function Page() {
           <h2 className="sr-only">Auctions</h2>
 
           <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {products.map((product, ind) => (
+            {products.slice(offset, offset + 8).map((product, ind) => (
               <a key={product.id} href={product.href} className="group">
                 <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <button
-                type="button"
-                onClick={() => show(ind)}>
-                  <img
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
-                    className="w-full h-60 object-center object-cover group-hover:opacity-75"
-                  />
+                  <button
+                    type="button"
+                    onClick={() => show(ind + offset)}>
+                    <img
+                      src={product.imageSrc}
+                      alt={product.imageAlt}
+                      className="w-full h-60 object-center object-cover group-hover:opacity-75"
+                    />
                   </button>
                 </div>
                 <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
@@ -148,86 +46,86 @@ export default function Page() {
         </div>
 
 
-        </div>
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={setOpen}>
-        <div className="flex min-h-screen text-center md:block md:px-2 lg:px-4" style={{ fontSize: 0 }}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="hidden fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity md:block" />
-          </Transition.Child>
+      </div>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={setOpen}>
+          <div className="flex min-h-screen text-center md:block md:px-2 lg:px-4" style={{ fontSize: 0 }}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="hidden fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity md:block" />
+            </Transition.Child>
 
-          {/* This element is to trick the browser into centering the modal contents. */}
-          <span className="hidden md:inline-block md:align-middle md:h-screen" aria-hidden="true">
-            &#8203;
-          </span>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
-            enterTo="opacity-100 translate-y-0 md:scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 md:scale-100"
-            leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
-          >
-            <div className="flex text-base text-left transform transition w-full md:inline-block md:max-w-2xl md:px-4 md:my-8 md:align-middle lg:max-w-4xl">
-              <div className="w-full relative flex items-center bg-white px-4 pt-14 pb-8 overflow-hidden shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
-                <button
-                  type="button"
-                  className="absolute top-4 right-4 text-gray-400 hover:text-gray-500 sm:top-8 sm:right-6 md:top-6 md:right-6 lg:top-8 lg:right-8"
-                  onClick={() => setOpen(false)}
-                >
-                  <span className="sr-only">Close</span>
-                  <XIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span className="hidden md:inline-block md:align-middle md:h-screen" aria-hidden="true">
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
+              enterTo="opacity-100 translate-y-0 md:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 md:scale-100"
+              leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
+            >
+              <div className="flex text-base text-left transform transition w-full md:inline-block md:max-w-2xl md:px-4 md:my-8 md:align-middle lg:max-w-4xl">
+                <div className="w-full relative flex items-center bg-white px-4 pt-14 pb-8 overflow-hidden shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                  <button
+                    type="button"
+                    className="absolute top-4 right-4 text-gray-400 hover:text-gray-500 sm:top-8 sm:right-6 md:top-6 md:right-6 lg:top-8 lg:right-8"
+                    onClick={() => setOpen(false)}
+                  >
+                    <span className="sr-only">Close</span>
+                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
 
-                <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
-                  <div className="aspect-w-2 aspect-h-3 rounded-lg bg-gray-100 overflow-hidden sm:col-span-4 lg:col-span-5">
-                    <img src={products[activeElement].imageSrc} alt={products[activeElement].imageAlt} className="object-center object-cover" />
-                  </div>
-                  <div className="sm:col-span-8 lg:col-span-7">
-                    <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">{products[activeElement].name}</h2>
+                  <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
+                    <div className="aspect-w-2 aspect-h-3 rounded-lg bg-gray-100 overflow-hidden sm:col-span-4 lg:col-span-5">
+                      <img src={products[activeElement].imageSrc} alt={products[activeElement].imageAlt} className="object-center object-cover" />
+                    </div>
+                    <div className="sm:col-span-8 lg:col-span-7">
+                      <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">{products[activeElement].name}</h2>
 
-                    <section aria-labelledby="information-heading" className="mt-2">
-                      <h3 id="information-heading" className="sr-only">
-                        Auction information
-                      </h3>
+                      <section aria-labelledby="information-heading" className="mt-2">
+                        <h3 id="information-heading" className="sr-only">
+                          Auction information
+                        </h3>
 
-                      <p className="text-2xl text-gray-900">{products[activeElement].description}</p>
-                      
-                    </section>
+                        <p className="text-2xl text-gray-900">{products[activeElement].description}</p>
 
-                    <section aria-labelledby="options-heading" className="mt-10">
-                      <h3 id="options-heading" className="sr-only">
-                         Bid options
-                      </h3>
+                      </section>
 
-                      <form>
-                    
-                        <button
-                          type="submit"
-                          className="mt-6 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                          Create bid 
-                        </button>
-                      </form>
-                    </section>
+                      <section aria-labelledby="options-heading" className="mt-10">
+                        <h3 id="options-heading" className="sr-only">
+                          Bid options
+                        </h3>
+
+                        <form>
+
+                          <button
+                            type="submit"
+                            className="mt-6 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                          >
+                            Create bid
+                          </button>
+                        </form>
+                      </section>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </Transition.Child>
-        </div>
-      </Dialog>
-    </Transition.Root>   
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </div>
-    
+
   )
 }
