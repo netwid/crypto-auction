7030671dab3f64d65af5b87978122a43a2907102
Kaladin13 <335095@niuitmo.ru>
2022-04-23 22:56:05 +0300

    Add basic tests

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,4 +1,4 @@
-import "@nomiclabs/hardhat-waffle";
+require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -16,4 +16,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-export const solidity = "0.8.4";
+module.exports = {
+  solidity: "0.8.4",
+};
