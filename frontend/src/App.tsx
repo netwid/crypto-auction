3127232b7900e26e1068c3444f6e1ac279200727
Dead <space.gee.z@gmail.com>
2022-04-23 19:41:16 +0300

    basic page added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,18 @@ import './App.css';
 import { Router, Route, Routes, Link } from 'react-router-dom';
 import { Main } from './pages/Main/Main';
 import { Web3 } from './pages/Web3/Web3';
-
+import Header from './components/Header';
+import Page from './components/Page';
+import Scroll from './components/Scroll';
 function App() {
   return (
+    
     <>
+    <Header />
+    <Page />
+    <Scroll />
+    
+    
       <ul>
         <li>
           <Link to="/">Main</Link>
