f139b671658d717f2618e79671e1b51f7a11eb16
Dead <space.gee.z@gmail.com>
2022-04-24 01:48:52 +0300

    img loader fix

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -184,26 +184,45 @@ export default function Header( props:{ setActivePage: Function, activePage: num
                                     style={{ border: "none", borderBottom: "2px solid #324054", outline: "0", color: "#000000" }}
                                 />
                            </div>
-                           <div className='flex justify-center mt-5'>
-                             
-                             <input 
-                             type="image"
-                             className="h-20 ml-7 w-60 bg-transparent border-2 border-grey"
-                             style={{ border: "none", borderBottom: "2px solid #324054", outline: "0", color: "#000000" }}/>
-                           </div>
+                           <div className="flex justify-center">
+                                <div className="">
+                                  <div className="m-4">
+                                      <label className="inline-block mb-2 text-black">Image Upload</label>
+                                      <div className="flex items-center justify-center w-60">
+                                          <label
+                                              className="flex flex-col w-full h-32 border-4 border-blue-200 border-dashed hover:bg-gray-100 hover:border-gray-300">
+                                              <div className="flex flex-col items-center justify-center pt-7">
+                                                  <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-gray-400 group-hover:text-gray-600"
+                                                      fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                          d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+                                                  </svg>
+                                                  <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
+                                                      Attach an image</p>
+                                              </div>
+                                              <input type="file" className="opacity-0" />
+                                          </label>
+                                        </div>
+                                      </div>
+                                  
+                                  </div>
+                              </div> 
+                           
                            
                         </div>
                           <div className='flex justify-center'>
                       
                             <button
                               type="submit"
-                              className="mt-6 bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                              className="bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
                               Create auction 
                             </button>
                           </div>
+                            
                           
                         </div>
+                        
                       </form>
                     </section>
                   </div>
