ad937b864fe48066f82617ec43c5d9ef2df29cd7
netwid <85802435+netwid@users.noreply.github.com>
2022-04-23 22:52:31 +0300

    Metamask (#2)
    * Base auth
    
    * Wallet component
    
    * Buy/Create components
    
    * All api handlers & some components

---
+++ frontend/src/components/Buy/Buy.tsx
@@ -0,0 +1,12 @@
+import { buyLot } from '../Contract';
+
+export const Buy = (props: {lotId: number}) => {
+    const buy = async () => {
+        const bid = Number.parseInt(prompt('Please enter bid') as string);
+        buyLot(props.lotId, bid);
+    }
+
+    return (
+        <button onClick={buy}>Buy lot</button>
+    )
+}
