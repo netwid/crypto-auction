17b5333064bb4dc2919988b81629ec7d3d6af9f2
Dead <space.gee.z@gmail.com>
2022-04-24 00:22:06 +0300

    f

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+
 
 let yourWallet = 'Lfs..Kjwe'
 let yourBalance = '123984'
@@ -17,7 +18,10 @@ export default function Header( props:{ setActivePage: Function, activePage: num
     { name: 'My lots', action: () => props.setActivePage(1), current: props.activePage == 1 }
   ]
 
+  const [open, setOpen] = useState(false);
+
   return (
+    <div>
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
@@ -61,7 +65,6 @@ export default function Header( props:{ setActivePage: Function, activePage: num
                   className="p-1"
                 >
                   Create new auction +
-
                 </button>
               </div>
 
@@ -94,6 +97,109 @@ export default function Header( props:{ setActivePage: Function, activePage: num
           </Disclosure.Panel> */}
         </>
       )}
-    </Disclosure>
+      </Disclosure>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={setOpen}>
+          <div className="flex min-h-screen text-center md:block md:px-2 lg:px-4" style={{ fontSize: 0 }}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="hidden fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity md:block" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span className="hidden md:inline-block md:align-middle md:h-screen" aria-hidden="true">
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
+              enterTo="opacity-100 translate-y-0 md:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 md:scale-100"
+              leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
+            >
+              <div className="flex text-base text-left transform transition w-full md:inline-block md:max-w-2xl md:px-4 md:my-8 md:align-middle lg:max-w-4xl">
+                <div className="w-full relative flex items-center bg-white px-4 pt-14 pb-8 overflow-hidden shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                  <button
+                    type="button"
+                    className="absolute top-4 right-4 text-gray-400 hover:text-gray-500 sm:top-8 sm:right-6 md:top-6 md:right-6 lg:top-8 lg:right-8"
+                    onClick={() => setOpen(false)}
+                  >
+                    <span className="sr-only">Close</span>
+                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+
+                  <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
+                    <div className="aspect-w-2 aspect-h-3 rounded-lg bg-gray-100 overflow-hidden sm:col-span-4 lg:col-span-5">
+                      <img src={} alt={} className="object-center object-cover" />
+                    </div>
+                    <div className="sm:col-span-8 lg:col-span-7">
+                      <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">{}</h2>
+
+                      <section aria-labelledby="information-heading" className="mt-2">
+                        <h3 id="information-heading" className="sr-only">
+                          Auction information
+                        </h3>
+
+                        <p className="text-2xl text-gray-900">{}</p>
+                        </section>
+
+                    <section aria-labelledby="options-heading" className="mt-4">
+                      <h3 id="options-heading" className="sr-only">
+                         Bid options
+                      </h3>
+
+                      
+                      
+                      <div className='text-2xl mb-3'>
+                        Time to end: {}
+                      </div>
+
+                      <form>
+                        <div className='bg-white text-2xl border-2 p-4 border-indigo-600 rounded-md'>
+                        
+                        <div>
+                          <div className='text-indigo-600 flex justify-center'>Set new bid</div>
+                          <div>Enter minimal value:
+                          <input
+                                    type="number"
+                                    placeholder="0.0"
+                                    className="h-7 ml-7 w-60 bg-transparent border-2 border-grey"
+                                    style={{ border: "none", borderBottom: "2px solid #324054", outline: "0", color: "#FFFFFF" }}
+                                />
+              
+                           </div>
+                        </div>
+                          <div className='flex justify-center'>
+                      
+                            <button
+                              type="submit"
+                              className="mt-6 bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            >
+                              Create auction
+                            </button>
+                          </div>
+                          
+                        </div>
+                      </form>
+                    </section>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+      </div>
+    
   )
 }
