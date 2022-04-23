5448b160a865917161e5dfc4aaaff1566da235ee
Netwid <me@netwid.dev>
2022-04-23 13:49:50 +0300

    Routes upd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,26 @@
 import './App.css';
-import { Router, Route, Routes } from 'react-router-dom';
+import { Router, Route, Routes, Link } from 'react-router-dom';
 import { Main } from './pages/Main/Main';
 import { Web3 } from './pages/Web3/Web3';
 
 function App() {
   return (
-    <Routes>
+    <>
+      <ul>
+        <li>
+          <Link to="/">Main</Link>
+        </li>
+        <li>
+          <Link to="/web3">Web3</Link>
+        </li>
+      </ul>
       <div>
-        <Route path="/" element={<Main />} />
-        <Route path="/web3" element={<Web3 />} />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/web3" element={<Web3 />} />
+        </Routes>
       </div>
-    </Routes>
+    </>
   );
 }
 
