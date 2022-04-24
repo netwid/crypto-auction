412ca9f49651cbeca01862d542ee41fea0f7a784
Dead <space.gee.z@gmail.com>
2022-04-24 08:31:05 +0300

    popup for auctions

--- frontend/src/components/Page.tsx
+++ frontend/src/components/Page.tsx
@@ -34,42 +34,14 @@ export default function Page(props: { products: Array<Product>, offset: number }
           Bid options
         </h3>
 
-
-
-        <div className='text-2xl mb-3'>
+        <div className='text-2xl mb-5'>
           Time to end: {timer.days} days {(timer.hours < 10 ? '0' : '') + timer.hours}:{(timer.minutes < 10 ? '0' : '') + timer.minutes}:{(timer.seconds < 10 ? '0' : '') + timer.seconds}
         </div>
-
-        <form>
-          <div className='bg-white text-2xl border-2 p-4 border-indigo-600 rounded-md'>
-
-            <div>
-              <div className='text-indigo-600 flex justify-center'>Set new bid</div>
-              <div>Minimal value: {props.products[activeElement].bid + 1}</div>
-              <div>Enter value:
-                <input
-                  type="number"
-                  placeholder="0"
-                  className="h-7 ml-7 w-60 bg-transparent border-2 border-grey"
-                  style={{ border: "none", borderBottom: "2px solid #324054", outline: "0", color: "#000000" }}
-                  min={props.products[activeElement].bid + 1}
-                />
-
-              </div>
-            </div>
-            <div className='flex justify-center'>
-
-              <button
-                type="submit"
-                className="mt-6 bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Create bid
-              </button>
-            </div>
-
-          </div>
-        </form>
       </section>
+      
+
+        
+      
     </>
   }
 
@@ -132,7 +104,8 @@ export default function Page(props: { products: Array<Product>, offset: number }
               leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
             >
               <div className="flex text-base text-left transform transition w-full md:inline-block md:max-w-2xl md:px-4 md:my-8 md:align-middle lg:max-w-4xl">
-                <div className="w-full relative flex items-center bg-white px-4 pt-14 pb-8 overflow-hidden shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                <div className="w-full relative flex-none rounded-lg items-center bg-white px-4 pt-14 pb-8 overflow-hidden shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                  <div className='flex'>
                   <button
                     type="button"
                     className="absolute top-4 right-4 text-gray-400 hover:text-gray-500 sm:top-8 sm:right-6 md:top-6 md:right-6 lg:top-8 lg:right-8"
@@ -143,25 +116,56 @@ export default function Page(props: { products: Array<Product>, offset: number }
                   </button>
 
                   <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
-                    <div className="aspect-w- aspect-h-3 rounded-lg bg-gray-100 overflow-hidden sm:col-span-4 lg:col-span-5">
+                    <div className="aspect-w-3 aspect-h-3 rounded-lg bg-gray-100 overflow-hidden sm:col-span-4 lg:col-span-5">
                       <img src={props.products[activeElement].imageSrc} alt={props.products[activeElement].imageAlt} className="object-center object-cover" />
-                    </div>
-                    <div className="sm:col-span-8 lg:col-span-7">
-                      <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">{props.products[activeElement].name}</h2>
+                      </div>
+                      <div className="sm:col-span-8 lg:col-span-7">
+                        <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">{props.products[activeElement].name}</h2>
 
                       <section aria-labelledby="information-heading" className="mt-2">
                         <h3 id="information-heading" className="sr-only">
                           Auction information
                         </h3>
 
-                        <p className="text-2xl text-gray-900">{props.products[activeElement].description}</p>
-                        </section>
+                        <p className="text-2xl text-gray-900 italic">"{props.products[activeElement].description}"</p>
+                      </section>
 
                         <Desc />
-
-                        
+                    </div>
+                    </div>
                   </div>
-                </div>
+                  <div> 
+                  <form>
+                    <div className='bg-white shadow-lg ml-40 mt-10 w-7/12 text-2xl border-2 p-4 border-gray-800 rounded-md'>
+                      <div>
+                        <div className='text-indigo-600 font-medium mb-4 flex justify-center'>Set new bid</div>
+                        <div className=''>Minimal value: {props.products[activeElement].bid + 1}</div>
+                        <div>Enter value:
+                          <input
+                            type="number"
+                            placeholder="0"
+                            className="h-7 ml-9 w-60 bg-transparent border-2 border-grey"
+                            style={{ border: "none", borderBottom: "2px solid #324054", outline: "0", color: "#000000" }}
+                            min={props.products[activeElement].bid + 1}
+                          />
+
+                        </div>
+                      </div>
+                      <div className='flex justify-center'>
+
+                        <button
+                          type="submit"
+                          className="mt-6 bg-indigo-600 shadow-lg border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                          Create bid
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                  </div>
+
+
+
                 </div>
               </div>
             </Transition.Child>
