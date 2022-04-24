97cb8ea36494aad4327fbdcdaaa2151fc1375ce2
Netwid <me@netwid.dev>
2022-04-24 09:55:59 +0300

    Hotfix

--- frontend/src/components/Page.tsx
+++ frontend/src/components/Page.tsx
@@ -38,11 +38,11 @@ export default function Page(props: { products: Array<Product>, offset: number }
   const Desc = () => {
     const timer = useTimer({ expiryTimestamp: new Date(props.products[activeElement].endTime), autoStart: true })
     return <>
-    <section className='mt-4 text-2xl'>
-      <div>Owner: {props.products[activeElement].owner}</div>
-      <div>Highest bid: {props.products[activeElement].bid}</div>
-      <div>Bidder: {props.products[activeElement].bidder}</div>
-    </section>
+      <section className='mt-4 text-2xl'>
+        <div>Owner: {props.products[activeElement].owner}</div>
+        <div>Highest bid: {props.products[activeElement].bid}</div>
+        <div>Bidder: {props.products[activeElement].bidder}</div>
+      </section>
       <section aria-labelledby="options-heading" className="mt-4">
         <h3 id="options-heading" className="sr-only">
           Bid options
@@ -119,56 +119,57 @@ export default function Page(props: { products: Array<Product>, offset: number }
                 <div className="w-full relative flex-none rounded-lg items-center bg-white px-4 pt-14
                  pb-8 overflow-hidden shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
                   <div className='flex'>
-                  <button
-                    type="button"
-                    className="absolute top-4 right-4 text-gray-400 hover:text-gray-500 sm:top-8 sm:right-6
+                    <button
+                      type="button"
+                      className="absolute top-4 right-4 text-gray-400 hover:text-gray-500 sm:top-8 sm:right-6
                      md:top-6 md:right-6 lg:top-8 lg:right-8"
-                    onClick={() => setOpen(false)}
-                  >
-                    <span className="sr-only">Close</span>
-                    <XIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
+                      onClick={() => setOpen(false)}
+                    >
+                      <span className="sr-only">Close</span>
+                      <XIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
 
-                  <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
+                    <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
 
-                    <div className="aspect-w-2 aspect-h-3 rounded-lg bg-gray-100 overflow-hidden sm:col-span-4 lg:col-span-5">
-                      {
-                        products1[activeElement] != null &&
-                        <img src={products1[activeElement].imageURL} alt={products1[activeElement].description} className="object-center object-cover" />
-                      }
-                    </div>
-                    <div className="sm:col-span-8 lg:col-span-7">
-                      <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">{props.products[activeElement].name}</h2>
-
-                      <section aria-labelledby="information-heading" className="mt-2">
-                        <h3 id="information-heading" className="sr-only">
-                          Auction information
-                        </h3>
-
-                        <p className="text-2xl text-gray-900">{props.products[activeElement].description}</p>
-                      </section>
-                      {
-                        products1[activeElement] != null &&
-                        <section className='mt-4 text-2xl'>
-                          <div>Owner: {products1[activeElement].owner.slice(2, 5) + '...' + products1[activeElement].owner.slice(-3)}</div>
-                          <div>Highest bid: {products1[activeElement].highestBid.toNumber()}</div>
-                          <div>Bidder: {products1[activeElement].highestBidder.toString().slice(2, 5) + '...' + products1[activeElement].highestBidder.toString().slice(-3)}</div>
-                        </section>
-                      }
-
-                      <section aria-labelledby="options-heading" className="mt-4">
-                        <h3 id="options-heading" className="sr-only">
-                          Bid options
-                        </h3>
-
-                        <div className='text-2xl mb-3'>
-                          Time to end: { }
-                        </div>
+                      <div className="aspect-w-2 aspect-h-3 rounded-lg bg-gray-100 overflow-hidden sm:col-span-4 lg:col-span-5">
                         {
                           products1[activeElement] != null &&
-                          <Buy lotId={products1[activeElement].id} />
+                          <img src={products1[activeElement].imageURL} alt={products1[activeElement].description} className="object-center object-cover" />
                         }
-                      </section>
+                      </div>
+                      <div className="sm:col-span-8 lg:col-span-7">
+                        <h2 className="text-2xl font-extrabold text-gray-900 sm:pr-12">{props.products[activeElement].name}</h2>
+
+                        <section aria-labelledby="information-heading" className="mt-2">
+                          <h3 id="information-heading" className="sr-only">
+                            Auction information
+                          </h3>
+
+                          <p className="text-2xl text-gray-900">{props.products[activeElement].description}</p>
+                        </section>
+                        {
+                          products1[activeElement] != null &&
+                          <section className='mt-4 text-2xl'>
+                            <div>Owner: {products1[activeElement].owner.slice(2, 5) + '...' + products1[activeElement].owner.slice(-3)}</div>
+                            <div>Highest bid: {products1[activeElement].highestBid.toNumber()}</div>
+                            <div>Bidder: {products1[activeElement].highestBidder.toString().slice(2, 5) + '...' + products1[activeElement].highestBidder.toString().slice(-3)}</div>
+                          </section>
+                        }
+
+                        <section aria-labelledby="options-heading" className="mt-4">
+                          <h3 id="options-heading" className="sr-only">
+                            Bid options
+                          </h3>
+
+                          <div className='text-2xl mb-3'>
+                            Time to end: { }
+                          </div>
+                          {
+                            products1[activeElement] != null &&
+                            <Buy lotId={products1[activeElement].id} />
+                          }
+                        </section>
+                      </div>
                     </div>
                   </div>
                 </div>
