b591b26370c45bc7373c15909aa98d3bbb84ba72
Netwid <me@netwid.dev>
2022-04-24 11:25:40 +0300

    Fix

--- frontend/src/components/LotAction/Close/Close.tsx
+++ frontend/src/components/LotAction/Close/Close.tsx
@@ -12,7 +12,7 @@ export const Close = (props: {lotId: BigNumber}) => {
             className="mt-6 bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             onClick={close}
         >
-            Create bid
+            Close auction
         </button>
     )
 } 
